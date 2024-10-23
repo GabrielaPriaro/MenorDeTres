750696b55fcfc10b6286ac303bf5975aa9b28c80
Gabriela <gabriela.priaro@gmail.com>
2024-10-23 12:40:41 +0100

    Implementaçao para mostrar menor numero de tres.

--- MenorDeTres/Program.cs
+++ MenorDeTres/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace MenorDeTres
+﻿//Problema "menor_de_tres"
+//Fazer um programa para ler três números inteiros. Em seguida, mostrar qual o menor dentre os três
+//números lidos. Em caso de empate, mostrar apenas uma vez.
+
+namespace MenorDeTres
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a, b, c, menor;
+
+            Console.Write("Primeiro valor: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Terceiro valor: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a < b && a < c)
+            {
+                menor = a;
+            }
+            else if (b < c)
+            {
+                menor = b;
+            }
+            else
+            {
+                menor = c;
+            }
+
+            Console.WriteLine($"MENOR = {menor}");
         }
     }
 }
